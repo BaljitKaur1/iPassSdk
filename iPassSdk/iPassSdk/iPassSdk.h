00548dac4f2a8d77c02f0515641be72f4b10aaf6
Baljit <baljitkaur@csgroupchd.com>
2023-01-23 13:45:48 +0530

    Added package file

---
+++ iPassSdk/iPassSdk/iPassSdk.h
@@ -0,0 +1,16 @@
+//
+//  iPassSdk.h
+//  iPassSdk
+//
+//  Created by CS on 23/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iPassSdk.
+FOUNDATION_EXPORT double iPassSdkVersionNumber;
+
+//! Project version string for iPassSdk.
+FOUNDATION_EXPORT const unsigned char iPassSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iPassSdk/PublicHeader.h>
